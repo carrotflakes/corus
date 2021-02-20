fdf17ed003b015f460ec4cc123e3bb4385c3a7fe
carrotflakes <carrotflakes@gmail.com>
2021-02-20 22:40:16 +0900

    update youkoso.rs

--- examples/youkoso.rs
+++ examples/youkoso.rs
@@ -1,4 +1,14 @@
-use corus::{node::{Node, accumulator::Accumulator, amp::Amp, constant::Constant, controllable::Controllable, pan::Pan, param::Param}, notenum_to_frequency, poly_synth::{PolySynth, Voice}, proc_context::ProcContext, signal::C1f32};
+use corus::{
+    contrib::{amp_pan, controllable_param},
+    node::{
+        accumulator::Accumulator, amp::Amp, constant::Constant, controllable::Controllable,
+        mix::Mix, pan::Pan, param::Param, Node,
+    },
+    notenum_to_frequency,
+    poly_synth::{PolySynth, Voice},
+    proc_context::ProcContext,
+    signal::{C1f32, C2f32},
+};
 
 fn main() {
     let sample_rate = 44100;
@@ -36,7 +46,14 @@ fn main() {
             }),
         )
     };
-    let mut synth = PolySynth::new(&builder, 32);
+    let mut tracks: Vec<_> = (0..20)
+        .map(|_| {
+            let synth = PolySynth::new(&builder, 16);
+            let (gain, gain_ctrl) = controllable_param(1.0);
+            let (pan, pan_ctrl) = controllable_param(0.0);
+            (synth, gain, pan, gain_ctrl, pan_ctrl)
+        })
+        .collect();
 
     let time = {
         let data = std::fs::read("youkoso.mid").unwrap();
@@ -44,35 +61,55 @@ fn main() {
         let mut time = 0.0;
         for e in ezmid::Dispatcher::new(events) {
             time = e.time;
+            let track = &mut tracks[e.event.channel as usize];
             match e.event.body {
                 ezmid::EventBody::NoteOn {
                     notenum,
                     velocity: _,
                     raw_velocity: _,
                 } => {
-                    synth.note_on(e.time, notenum_to_frequency(notenum as u32));
+                    track
+                        .0
+                        .note_on(e.time, notenum_to_frequency(notenum as u32));
                 }
                 ezmid::EventBody::NoteOff {
                     notenum,
                     velocity: _,
                     raw_velocity: _,
                 } => {
-                    synth.note_off(e.time, notenum_to_frequency(notenum as u32));
+                    track
+                        .0
+                        .note_off(e.time, notenum_to_frequency(notenum as u32));
                 }
-                _ => {}
+                ezmid::EventBody::Volume { volume, .. } => {
+                    track.3.lock().set_value_at_time(e.time, volume);
+                }
+                ezmid::EventBody::Pan { pan, .. } => {
+                    track.4.lock().set_value_at_time(e.time, pan);
+                }
+                ezmid::EventBody::PitchBend { bend, raw_bend } => {}
+                ezmid::EventBody::Tempo { tempo } => {}
             }
         }
         time + 1.0
     };
 
-    let mut node = Amp::new(synth, Constant::new(C1f32::from(0.1)));
-    let mut node = Pan::new(node, Constant::from(-0.5));
+    let mix = Mix::new(
+        tracks
+            .into_iter()
+            .map(|t| Box::new(amp_pan(t.0, t.1, t.2)) as Box<dyn Node<C2f32>>)
+            .collect(),
+    );
+    let mut node = Amp::new(mix, Constant::new(C2f32([0.25, 0.25])));
 
     let pc = ProcContext::new(sample_rate);
     let mut writer = Writer::new("youkoso.wav");
     let start = std::time::Instant::now();
     node.lock();
-    for s in pc.into_iter(&mut node).take((sample_rate as f64 * time) as usize) {
+    for s in pc
+        .into_iter(&mut node)
+        .take((sample_rate as f64 * time) as usize)
+    {
         writer.write(s.0[0], s.0[1]);
     }
     node.unlock();
