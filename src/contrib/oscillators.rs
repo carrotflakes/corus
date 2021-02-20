2aef5d1b37a18185d8f8b2985c93518a36488ad2
carrotflakes <carrotflakes@gmail.com>
2021-02-21 00:47:38 +0900

    resetable_acc

--- src/contrib/oscillators.rs
+++ src/contrib/oscillators.rs
@@ -1,10 +1,5 @@
 use crate::{
-    node::{
-        accumulator::Accumulator,
-        controllable::Controllable,
-        map::Map,
-        Node,
-    },
+    node::{accumulator::Accumulator, controllable::Controllable, map::Map, Node},
     signal::C1f32,
 };
 
@@ -37,4 +32,17 @@ pub fn retriggerable_saw<A: Node<C1f32> + 'static, DA: AsMut<A> + 'static>(
         Map::new(acc, |v| C1f32::from((v.0[0] - 0.5) * 2.0)),
         move |time: f64| acc_ctrl.lock().set_value_at_time(time, 0.0.into()),
     )
+}
+
+pub fn resetable_acc<A: Node<C1f32> + 'static, DA: AsMut<A> + 'static>(
+    frequency: DA,
+) -> (
+    Controllable<C1f32, Accumulator<C1f32, A, DA>>,
+    impl FnMut(f64, f32),
+) {
+    let acc = Controllable::new(Accumulator::new(frequency, C1f32::from(1.0)));
+    let mut acc_ctrl = acc.controller();
+    (acc, move |time: f64, value: f32| {
+        acc_ctrl.lock().set_value_at_time(time, value.into())
+    })
 }
