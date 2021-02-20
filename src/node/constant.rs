eb5ee33ea084e8d11bf50b4e7e6f3a3aa438fc0e
carrotflakes <carrotflakes@gmail.com>
2021-02-21 00:00:19 +0900

    inline Constant::proc

--- src/node/constant.rs
+++ src/node/constant.rs
@@ -16,6 +16,7 @@ impl<T: Clone + 'static> Constant<T> {
 }
 
 impl<T: Clone + 'static> Node<T> for Constant<T> {
+    #[inline]
     fn proc(&mut self, _ctx: &ProcContext) -> T {
         self.value.clone()
     }
