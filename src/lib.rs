a3f8068e0edde28c5985be15f1923eec12b132d4
carrotflakes <carrotflakes@gmail.com>
2021-02-22 03:32:27 +0900

    remove empty test

--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,3 @@ pub mod signal;
 pub fn notenum_to_frequency(notenum: u32) -> f32 {
     440.0 * 2.0f32.powf((notenum as f32 - 69.0) / 12.0)
 }
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
