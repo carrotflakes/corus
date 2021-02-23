6c644046e386ee18a55905080677e49c9f14275e
carrotflakes <carrotflakes@gmail.com>
2021-02-23 21:43:48 +0900

    benihora waveform

--- src/contrib/benihora/glottis.rs
+++ src/contrib/benihora/glottis.rs
@@ -8,29 +8,19 @@ pub struct Glottis {
 
     total_time: F,
     time_in_waveform: F,
-    waveform_length: F,
+
+    waveform: Waveform,
 
     intensity: F,
     loudness: F,
 
     is_touched: bool,
     is_touching_somewhere: bool,
-    //ctx: backCtx,
-    // touch: F,
-    // x: F,
-    // y: F,
-    alpha: F,
-    e0: F,
-    epsilon: F,
-    shift: F,
-    delta: F,
-    te: F,
-    omega: F,
 }
 
 impl Glottis {
     pub fn new() -> Self {
-        let mut glottis = Self {
+        Self {
             frequency: FrequencyCtrl::new(140.0, 0.005, 6.0, false),
             tenseness: TensenessCtrl::new(0.6),
 
@@ -41,18 +31,8 @@ impl Glottis {
             is_touching_somewhere: false,
 
             time_in_waveform: 0.0,
-            waveform_length: 0.0,
-
-            alpha: 0.0,
-            e0: 0.0,
-            epsilon: 0.0,
-            shift: 0.0,
-            delta: 0.0,
-            te: 0.0,
-            omega: 0.0,
-        };
-        glottis.setup_waveform(0.0);
-        glottis
+            waveform: Waveform::new(140.0, 0.6),
+        }
     }
 
     pub fn run_step(&mut self, sample_rate: usize, lambda: F, input: F) -> F {
@@ -62,11 +42,15 @@ impl Glottis {
         let time_step = 1.0 / sample_rate as F;
         self.time_in_waveform += time_step;
         self.total_time += time_step;
-        if self.waveform_length < self.time_in_waveform {
-            self.time_in_waveform -= self.waveform_length;
-            self.setup_waveform(lambda);
+        if self.waveform.length < self.time_in_waveform {
+            self.time_in_waveform -= self.waveform.length;
+            self.waveform = Waveform::new(self.frequency.get(lambda), self.tenseness.get(lambda))
         }
-        let out = self.normalized_lf_waveform(self.time_in_waveform / self.waveform_length);
+        let out = self.intensity
+            * self.loudness
+            * self
+                .waveform
+                .normalized_lf_waveform(self.time_in_waveform / self.waveform.length);
         let aspiration = self.intensity
             * (1.0 - self.tenseness.ui_tenseness.sqrt())
             * self.get_noise_modulator()
@@ -77,7 +61,7 @@ impl Glottis {
 
     pub fn get_noise_modulator(&mut self) -> F {
         let voiced =
-            0.1 + 0.2 * 0.0f64.max((PI * 2.0 * self.time_in_waveform / self.waveform_length).sin());
+            0.1 + 0.2 * 0.0f64.max((PI * 2.0 * self.time_in_waveform / self.waveform.length).sin());
         self.tenseness.ui_tenseness * self.intensity * voiced
             + (1.0 - self.tenseness.ui_tenseness * self.intensity) * 0.3
     }
@@ -98,63 +82,6 @@ impl Glottis {
             self.intensity -= block_time * 5.0
         }
         self.intensity = self.intensity.clamp(0.0, 1.0);
-    }
-
-    fn setup_waveform(&mut self, lambda: F) {
-        let frequency = self.frequency.get(lambda);
-        let tenseness = self.tenseness.get(lambda);
-
-        let rd = (3.0 * (1.0 - tenseness)).clamp(0.5, 2.7);
-        self.waveform_length = 1.0 / frequency;
-
-        let ra = -0.01 + 0.048 * rd;
-        let rk = 0.224 + 0.118 * rd;
-        let rg = (rk / 4.0) * (0.5 + 1.2 * rk) / (0.11 * rd - ra * (0.5 + 1.2 * rk));
-
-        let ta = ra;
-        let tp = 1.0 / (2.0 * rg);
-        let te = tp + tp * rk;
-
-        let epsilon = 1.0 / ta;
-        let shift = (-epsilon * (1.0 - te)).exp();
-        let delta = 1.0 - shift; //divide by this to scale RHS
-
-        let rhs_integral = ((1.0 / epsilon) * (shift - 1.0) + (1.0 - te) * shift) / delta;
-
-        let total_lower_integral = -(te - tp) / 2.0 + rhs_integral;
-        let total_upper_integral = -total_lower_integral;
-
-        let omega = PI / tp;
-        let s = (omega * te).sin();
-        // need E0*e^(alpha*Te)*s = -1 (to meet the return at -1)
-        // and E0*e^(alpha*Tp/2) * Tp*2/pi = totalUpperIntegral
-        //             (our approximation of the integral up to Tp)
-        // writing x for e^alpha,
-        // have E0*x^Te*s = -1 and E0 * x^(Tp/2) * Tp*2/pi = totalUpperIntegral
-        // dividing the second by the first,
-        // letting y = x^(Tp/2 - Te),
-        // y * Tp*2 / (pi*s) = -totalUpperIntegral;
-        let y = -PI * s * total_upper_integral / (tp * 2.0);
-        let z = y.ln();
-        let alpha = z / (tp / 2.0 - te);
-        let e0 = -1.0 / (s * (alpha * te).exp());
-        self.alpha = alpha;
-        self.e0 = e0;
-        self.epsilon = epsilon;
-        self.shift = shift;
-        self.delta = delta;
-        self.te = te;
-        self.omega = omega;
-    }
-
-    fn normalized_lf_waveform(&self, t: F) -> F {
-        self.intensity
-            * self.loudness
-            * if self.te < t {
-                (-(-self.epsilon * (t - self.te)).exp() + self.shift) / self.delta
-            } else {
-                self.e0 * (self.alpha * t).exp() * (self.omega * t).sin()
-            }
     }
 }
 
@@ -233,5 +160,75 @@ impl TensenessCtrl {
 
     fn get(&self, lambda: F) -> F {
         self.old_tenseness * (1.0 - lambda) + self.new_tenseness * lambda
+    }
+}
+
+struct Waveform {
+    length: F,
+
+    alpha: F,
+    e0: F,
+    epsilon: F,
+    shift: F,
+    delta: F,
+    te: F,
+    omega: F,
+}
+
+impl Waveform {
+    fn new(frequency: F, tenseness: F) -> Self {
+        let rd = (3.0 * (1.0 - tenseness)).clamp(0.5, 2.7);
+        let waveform_length = 1.0 / frequency;
+
+        let ra = -0.01 + 0.048 * rd;
+        let rk = 0.224 + 0.118 * rd;
+        let rg = (rk / 4.0) * (0.5 + 1.2 * rk) / (0.11 * rd - ra * (0.5 + 1.2 * rk));
+
+        let ta = ra;
+        let tp = 1.0 / (2.0 * rg);
+        let te = tp + tp * rk;
+
+        let epsilon = 1.0 / ta;
+        let shift = (-epsilon * (1.0 - te)).exp();
+        let delta = 1.0 - shift; //divide by this to scale RHS
+
+        let rhs_integral = ((1.0 / epsilon) * (shift - 1.0) + (1.0 - te) * shift) / delta;
+
+        let total_lower_integral = -(te - tp) / 2.0 + rhs_integral;
+        let total_upper_integral = -total_lower_integral;
+
+        let omega = PI / tp;
+        let s = (omega * te).sin();
+        // need E0*e^(alpha*Te)*s = -1 (to meet the return at -1)
+        // and E0*e^(alpha*Tp/2) * Tp*2/pi = totalUpperIntegral
+        //             (our approximation of the integral up to Tp)
+        // writing x for e^alpha,
+        // have E0*x^Te*s = -1 and E0 * x^(Tp/2) * Tp*2/pi = totalUpperIntegral
+        // dividing the second by the first,
+        // letting y = x^(Tp/2 - Te),
+        // y * Tp*2 / (pi*s) = -totalUpperIntegral;
+        let y = -PI * s * total_upper_integral / (tp * 2.0);
+        let z = y.ln();
+        let alpha = z / (tp / 2.0 - te);
+        let e0 = -1.0 / (s * (alpha * te).exp());
+
+        Self {
+            length: waveform_length,
+            alpha,
+            e0,
+            epsilon,
+            shift,
+            delta,
+            te,
+            omega,
+        }
+    }
+
+    fn normalized_lf_waveform(&self, t: F) -> F {
+        if self.te < t {
+            (-(-self.epsilon * (t - self.te)).exp() + self.shift) / self.delta
+        } else {
+            self.e0 * (self.alpha * t).exp() * (self.omega * t).sin()
+        }
     }
 }
