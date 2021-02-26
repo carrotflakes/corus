afdc9a59b81786da503461cc84bf08106cd84198
carrotflakes <carrotflakes@gmail.com>
2021-02-26 23:55:20 +0900

    show hash in write_to_file

--- examples/write_to_file.rs
+++ examples/write_to_file.rs
@@ -1,4 +1,6 @@
-use corus::{Node, ProcContext, signal::{Stereo, IntoStereo}};
+use std::{collections::hash_map::DefaultHasher, hash::Hasher};
+
+use corus::{Node, ProcContext, signal::{IntoStereo, Stereo}};
 
 pub fn write_to_file<T: IntoStereo<f64>, N: Node<T>, DN: AsMut<N>>(
     name: &str,
@@ -14,21 +16,31 @@ pub fn write_to_file<T: IntoStereo<f64>, N: Node<T>, DN: AsMut<N>>(
     };
     let mut writer = hound::WavWriter::create(name, spec).unwrap();
     let mut pc = ProcContext::new(sample_rate as u64);
+    let mut f64hasher = DefaultHasher::new();
+    let mut i16hasher = DefaultHasher::new();
     let start = std::time::Instant::now();
     for s in pc
         .lock(&mut node)
         .take((sample_rate as f64 * len).ceil() as usize)
     {
         let s = s.into_stereo();
+        let l = (s.get_l() * std::i16::MAX as f64) as i16;
+        let r = (s.get_r() * std::i16::MAX as f64) as i16;
+        f64hasher.write(&s.get_l().to_le_bytes());
+        f64hasher.write(&s.get_r().to_le_bytes());
+        i16hasher.write_i16(l);
+        i16hasher.write_i16(r);
         writer
-            .write_sample((s.get_l() * std::i16::MAX as f64) as i16)
+            .write_sample(l)
             .unwrap();
         writer
-            .write_sample((s.get_r() * std::i16::MAX as f64) as i16)
+            .write_sample(r)
             .unwrap();
     }
     writer.finalize().unwrap();
     println!("{:?} elapsed", start.elapsed());
+    println!("hash(f64): {:x}", f64hasher.finish());
+    println!("hash(i16): {:x}", i16hasher.finish());
 }
 
 #[allow(dead_code)]
