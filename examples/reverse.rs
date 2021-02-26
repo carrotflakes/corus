446b3a1c4f7e3d49a54606d3d066acce3c334e40
carrotflakes <carrotflakes@gmail.com>
2021-02-27 00:38:49 +0900

    add examples/reverse.rs

---
+++ examples/reverse.rs
@@ -0,0 +1,23 @@
+mod write_to_file;
+
+use corus::core::{accumulator::Accumulator, constant::Constant, map::Map};
+
+const SAMPLE_RATE: usize = 44100;
+
+fn main() {
+    let file = std::env::args()
+        .skip(1)
+        .next()
+        .unwrap_or("beats.wav".to_string());
+    println!("load {:?} ...", &file);
+    let buf = write_to_file::read_wav_file(&file);
+
+    let acc = Accumulator::new(Constant::from(-1.0), 100.0);
+    let node = Map::new(acc, |f| {
+        buf[((f * SAMPLE_RATE as f64) as usize).rem_euclid(buf.len())]
+    });
+    let file = format!("{}-reversed.wav", file[..file.len() - 4].to_string());
+
+    write_to_file::write_to_file(file.as_str(), 44100, 5.0, node);
+    println!("saved {:?}", &file);
+}
