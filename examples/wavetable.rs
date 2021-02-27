bf9e87dc57bdce5f1365ea913cd6dca328c360f5
carrotflakes <carrotflakes@gmail.com>
2021-02-28 02:27:28 +0900

    add exmples/wavetable.rs

---
+++ examples/wavetable.rs
@@ -0,0 +1,30 @@
+mod write_to_file;
+
+use corus::core::{
+    accumulator::Accumulator,
+    biquad_filter::{BiquadFilter, BiquadFilterParams, LowPass},
+    constant::Constant,
+    map::Map,
+};
+
+const SAMPLE_RATE: usize = 44100;
+
+fn main() {
+    let buf = vec![0.0, 0.2, 0.5, 0.3, 0.4, 0.3, 0.3, 0.1, 0.1, 0.1, -1.0];
+    let acc = Accumulator::new(Constant::from(-1.0), 1.0);
+    let node = Map::new(acc, move |f| {
+        buf[((f * buf.len() as f64 * 140.0) as usize).rem_euclid(buf.len())]
+    });
+    // let node = Smooth::new(node, 0.9);
+    let node = BiquadFilter::new(
+        node,
+        BiquadFilterParams::new(
+            LowPass,
+            Constant::from(500.0),
+            Constant::from(0.0),
+            Constant::from(1.0),
+        ),
+    );
+
+    write_to_file::write_to_file("wavetable.wav", SAMPLE_RATE, 3.0, node);
+}
