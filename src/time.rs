78846d013ce94584fb0f6d01156ef877744466a7
carrotflakes <carrotflakes@gmail.com>
2021-03-05 02:05:53 +0900

    Hz

--- src/time.rs
+++ src/time.rs
@@ -4,6 +4,9 @@ pub struct Sample(pub u64);
 #[derive(Debug, Clone, Copy)]
 pub struct Second(pub f64);
 
+#[derive(Debug, Clone, Copy)]
+pub struct Hz(pub f64);
+
 pub trait AsSample {
     fn as_sample(&self, sample_rate: u64) -> u64;
 }
@@ -37,5 +40,19 @@ impl AsSecond for Second {
     #[inline]
     fn as_second(&self, _sample_rate: u64) -> f64 {
         self.0
+    }
+}
+
+impl AsSample for Hz {
+    #[inline]
+    fn as_sample(&self, sample_rate: u64) -> u64 {
+        (sample_rate as f64 / self.0 as f64) as u64
+    }
+}
+
+impl AsSecond for Hz {
+    #[inline]
+    fn as_second(&self, _sample_rate: u64) -> f64 {
+        1.0 / self.0
     }
 }
