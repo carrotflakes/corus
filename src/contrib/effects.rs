1589f255e4209edde50ff3b286e953cf78f9a385
carrotflakes <carrotflakes@gmail.com>
2021-11-20 03:38:10 +0900

    generic delay_fx

--- src/contrib/effects.rs
+++ src/contrib/effects.rs
@@ -1,21 +1,26 @@
 use crate::{
     core::{
-        add::Add, amp::Amp, var::Var, placeholder::Placeholder,
-        ring_buffer_playback::RingBufferPlayback, ring_buffer_record::RingBufferRecord,
-        share::Share, Node,
+        add::Add, amp::Amp, placeholder::Placeholder, ring_buffer_playback::RingBufferPlayback,
+        ring_buffer_record::RingBufferRecord, share::Share, var::Var, Node,
     },
-    signal::C2f64,
+    signal::Signal,
 };
 
-pub fn delay_fx<A: Node<Output = C2f64> + 'static + Send + Sync>(
+pub fn delay_fx<
+    S: Signal<Float = f64> + Sync + Send,
+    A: Node<Output = S> + 'static + Send + Sync,
+>(
     node: A,
     sample_rate: usize,
     delay: f64,
     feedback: f64,
-) -> Share<RingBufferRecord<Placeholder<Box<dyn Node<Output = C2f64> + Send + Sync>>>> {
+) -> Share<RingBufferRecord<Placeholder<Box<dyn Node<Output = S> + Send + Sync>>>> {
     let mut p = Placeholder::new(None);
     let mut ps = p.setter();
-    let buffer = Share::new(RingBufferRecord::new(p, sample_rate));
+    let buffer = Share::new(RingBufferRecord::new(
+        p,
+        (sample_rate as f64 * delay) as usize + 1,
+    ));
     unsafe {
         ps.set(Box::new(Add::new(
             node,
@@ -23,7 +28,7 @@ pub fn delay_fx<A: Node<Output = C2f64> + 'static + Send + Sync>(
                 RingBufferPlayback::new(Var::from(delay), buffer.clone()),
                 Var::from(feedback),
             ),
-        )) as Box<dyn Node<Output = C2f64> + Send + Sync>);
+        )) as Box<dyn Node<Output = S> + Send + Sync>);
     }
     buffer
 }
