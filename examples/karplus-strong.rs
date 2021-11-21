61f48f5bff1c58fda9bec009e2bc6a474cdd37ce
carrotflakes <carrotflakes@gmail.com>
2021-11-22 00:06:38 +0900

    update karplus-strong.ts

--- examples/karplus-strong.rs
+++ examples/karplus-strong.rs
@@ -3,7 +3,7 @@ mod write_to_file;
 use corus::{
     contrib::{amp_pan, chip::Noise},
     core::{
-        add::Add, amp::Amp, first_order_filter::FirstOrderFilter, param::Param,
+        add::Add, amp::Amp, first_order_filter::FirstOrderFilter, mul::Mul, param::Param,
         placeholder::Placeholder, ring_buffer_playback::RingBufferPlayback,
         ring_buffer_record::RingBufferRecord, share::Share, var::Var, Node,
     },
@@ -17,32 +17,32 @@ fn main() {
     let node = Noise::new();
     let mut env = Param::new();
     env.set_value_at_time(0.0, 1.0);
-    env.set_value_at_time(0.001, 0.0);
+    env.set_value_at_time(0.01, 0.0);
     env.set_value_at_time(0.5, 1.0);
-    env.set_value_at_time(0.501, 0.0);
+    env.set_value_at_time(0.51, 0.0);
     env.set_value_at_time(1.0, 1.0);
-    env.set_value_at_time(1.001, 0.0);
+    env.set_value_at_time(1.01, 0.0);
     env.set_value_at_time(1.5, 1.0);
-    env.set_value_at_time(1.501, 0.0);
-    let node = Amp::new(node, env);
+    env.set_value_at_time(1.51, 0.0);
+    env.set_value_at_time(3.0, 0.0);
+    env.linear_ramp_to_value_at_time(4.0, 0.1);
+    env.linear_ramp_to_value_at_time(7.0, 0.0);
+    let noise = Amp::new(node, env);
+    let noise = Share::new(noise);
 
     let mut delay = Param::new();
-    delay.set_value_at_time(
-        0.0,
-        1.0 / notenum_to_frequency(60) - 0.5 / SAMPLE_RATE as f64,
-    );
-    delay.set_value_at_time(
-        0.5,
-        1.0 / notenum_to_frequency(64) - 0.5 / SAMPLE_RATE as f64,
-    );
-    delay.set_value_at_time(
-        1.0,
-        1.0 / notenum_to_frequency(67) - 0.5 / SAMPLE_RATE as f64,
-    );
-    delay.set_value_at_time(
-        1.5,
-        1.0 / notenum_to_frequency(72) - 0.5 / SAMPLE_RATE as f64,
-    );
+    let f = |nn: u8| 1.0 / notenum_to_frequency(nn) - 0.5 / SAMPLE_RATE as f64;
+    delay.set_value_at_time(0.0, f(60));
+    delay.set_value_at_time(0.5, f(64));
+    delay.set_value_at_time(1.0, f(67));
+    delay.set_value_at_time(1.5, f(72));
+    delay.set_value_at_time(4.5, f(71));
+    delay.set_value_at_time(4.75, f(69));
+    delay.set_value_at_time(5.0, f(67));
+    delay.set_value_at_time(5.25, f(65));
+    delay.set_value_at_time(5.5, f(64));
+    delay.set_value_at_time(5.75, f(62));
+    delay.set_value_at_time(6.0, f(60));
 
     let node = {
         let mut p = Placeholder::new(None);
@@ -50,7 +50,7 @@ fn main() {
         let buffer = Share::new(RingBufferRecord::new(p, SAMPLE_RATE));
         unsafe {
             ps.set(Box::new(Add::new(
-                node,
+                noise.clone(),
                 Amp::new(
                     FirstOrderFilter::new(
                         RingBufferPlayback::new(delay, buffer.clone()),
@@ -62,6 +62,10 @@ fn main() {
         }
         buffer
     };
+
+    // cancel original noise
+    let node = Add::new(node, Mul::new(noise, Var::from(-1.0)));
+
     let node = amp_pan(node, Var::from(0.5), Var::from(0.0));
 
     write_to_file::write_to_file("karplus-strong.wav", SAMPLE_RATE, 10.0, node, None, None);
