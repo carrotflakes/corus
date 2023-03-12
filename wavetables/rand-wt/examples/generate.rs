0e7bb9324d5f1304cc0e5b1d38a3a860306ec3da
carrotflakes <carrotflakes@gmail.com>
2023-03-13 00:46:18 +0900

    rand-wt crate

---
+++ wavetables/rand-wt/examples/generate.rs
@@ -0,0 +1,30 @@
+fn main() {
+    let sample_rate = 96000;
+
+    let mut rng: rand::rngs::StdRng = rand::SeedableRng::from_seed([0; 32]);
+
+    let dir = "./generated";
+    std::fs::create_dir(dir);
+
+    for i in 0..100 {
+        let wt = rand_wt::Config { least_depth: 2 }.generate(&mut rng);
+        let wt = wt.build();
+
+        let name = format!("{}/{:>02}.wav", dir, i);
+        let spec = hound::WavSpec {
+            channels: 1,
+            sample_rate,
+            bits_per_sample: 16,
+            sample_format: hound::SampleFormat::Int,
+        };
+        let mut writer = hound::WavWriter::create(name, spec).unwrap();
+        let n = sample_rate / 440;
+        for i in 0..n {
+            let x = wt(i as f64 / n as f64);
+            writer
+                .write_sample((x * std::i16::MAX as f64) as i16)
+                .unwrap();
+        }
+        writer.finalize().unwrap();
+    }
+}
---
+++ wavetables/rand-wt/src/lib.rs
@@ -0,0 +1,119 @@
+use wavetables::tree::Tree;
+
+#[derive(Debug, Clone)]
+pub struct Config {
+    pub least_depth: usize,
+}
+
+impl Config {
+    pub fn generate(&self, rng: &mut impl rand::Rng) -> Tree {
+        Generator::new(self, rng).generate()
+    }
+}
+
+pub struct Generator<'a, R: rand::Rng> {
+    config: &'a Config,
+    rng: &'a mut R,
+}
+
+impl<'a, R: rand::Rng> Generator<'a, R> {
+    pub fn new(config: &'a Config, rng: &'a mut R) -> Self {
+        Self { config, rng }
+    }
+
+    pub fn generate(&mut self) -> Tree {
+        self.generate_(0)
+    }
+
+    fn generate_(&mut self, depth: usize) -> Tree {
+        if depth < self.config.least_depth {
+            self.generate_branch(depth)
+        } else {
+            if self.rng.gen_bool(0.5) {
+                self.generate_branch(depth)
+            } else {
+                self.generate_leaf()
+            }
+        }
+    }
+
+    fn generate_leaf(&mut self) -> Tree {
+        if self.rng.gen_bool(0.2) {
+            return Tree::Sin;
+        }
+        match self.rng.gen_range(0..=6) {
+            0 => Tree::Sin,
+            1 => Tree::Triangle,
+            2 => Tree::ShiftedTriangle,
+            3 => Tree::Saw,
+            4 => Tree::Square,
+            5 => Tree::Pulse(self.rng.gen_range(0.0..1.0)),
+            6 => Tree::Quadratic,
+            _ => unreachable!(),
+        }
+    }
+
+    fn generate_branch(&mut self, depth: usize) -> Tree {
+        match self.rng.gen_range(0..=7) {
+            0 => Tree::Negative(Box::new(self.generate_(depth + 1))),
+            1 => Tree::Reversed(Box::new(self.generate_(depth + 1))),
+            2 => Tree::Join(
+                Box::new(self.generate_(depth + 1)),
+                Box::new(self.generate_(depth + 1)),
+            ),
+            3 => Tree::Shift(
+                if self.rng.gen_bool(0.3) {
+                    match self.rng.gen_range(1..=5) {
+                        1 => 0.25,
+                        2 => 0.5,
+                        3 => 0.75,
+                        4 => 1.0 / 3.0,
+                        5 => 2.0 / 3.0,
+                        _ => unreachable!(),
+                    }
+                } else {
+                    self.rng.gen_range(0.0..1.0)
+                },
+                Box::new(self.generate_(depth + 1)),
+            ),
+            4 => Tree::Scale(
+                if self.rng.gen_bool(0.7) {
+                    match self.rng.gen_range(1..=9) {
+                        1 => 0.25,
+                        2 => 0.5,
+                        3 => 0.75,
+                        4 => 1.0 / 3.0,
+                        5 => 2.0 / 3.0,
+                        6 => 1.5,
+                        7 => 2.0,
+                        8 => 3.0,
+                        9 => 4.0,
+                        _ => unreachable!(),
+                    }
+                } else {
+                    self.rng.gen_range(0.0..=4.0)
+                },
+                Box::new(self.generate_(depth + 1)),
+            ),
+            5 => Tree::Blend(
+                if self.rng.gen_bool(0.3) {
+                    0.5
+                } else {
+                    self.rng.gen_range(0.0..1.0)
+                },
+                Box::new(self.generate_(depth + 1)),
+                Box::new(self.generate_(depth + 1)),
+            ),
+            6 => Tree::DynamicBlend(
+                Box::new(self.generate_(depth + 1)),
+                Box::new(self.generate_(depth + 1)),
+                Box::new(self.generate_(depth + 1)),
+            ),
+            7 => Tree::Dot(
+                Box::new(self.generate_(depth + 1)),
+                Box::new(self.generate_(depth + 1)),
+            ),
+            _ => unreachable!(),
+        }
+    }
+}
