339259e6b9e0d22b406ee1196abc08b71744a124
carrotflakes <carrotflakes@gmail.com>
2023-03-15 01:26:03 +0900

    v2 fix Unison

--- v2/src/nodes/unison.rs
+++ v2/src/nodes/unison.rs
@@ -16,6 +16,10 @@ impl Unison {
         }
     }
 
+    pub fn set_voice_num(&mut self, n: usize) {
+        self.phases.resize_with(n, Phase::new);
+    }
+
     pub fn reset(&mut self) {
         for phase in self.phases.iter_mut() {
             phase.set(0.0);
@@ -31,13 +35,22 @@ impl Unison {
         f: impl Fn(f64) -> f64,
     ) -> StereoF64 {
         let n = self.phases.len();
-        let scale = 1.0 / n as f64;
+        let scale = 1.0 / (n as f64).sqrt();
         let mut x = StereoF64::default();
         for (i, phase) in self.phases.iter_mut().enumerate() {
-            let frequency = frequency * (1.0 + detune * (i as f64 / n as f64 - 0.5));
+            let detune_amount = if n == 1 {
+                0.0
+            } else {
+                detune * (i as f64 / n as f64 - 0.5)
+            };
+            let frequency = frequency * (1.0 + detune_amount);
             let phase = phase.process(ctx, frequency);
             let y = f(phase) * scale;
-            let pan = (i as f64 / n as f64 - 0.5) * 2.0 * stereo_width;
+            let pan = if n == 1 {
+                0.0
+            } else {
+                (i as f64 / (n - 1) as f64 - 0.5) * 2.0 * stereo_width
+            };
             x = x.add(y.into_stereo_with_pan(pan));
         }
         x
